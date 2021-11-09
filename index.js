09410f60c890bc68d618d2856f7a0d80026d9b59
flcsn <flacson98@gmail.com>
2021-11-09 15:39:59 +0800

    completed exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+  const method = tokens.method(req, res)
+  return [
+    method,
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    method === 'POST' ? JSON.stringify(req.body) : null
+  ].join(' ')
+}))
 
 let persons = [
   { 
